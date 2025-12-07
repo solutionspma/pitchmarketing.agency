cb5718baf113ec26aee069fb9d2fb2207e5c540a
CFFS Mac Mini <cffsmacmini@CFFSs-Mac-mini.local>
2025-12-07 11:55:27 -0600

    Add Pexels image support and Print Shop page
    - Fixed next.config.js to allow images.pexels.com
    - Created full /print page with 8 product categories
    - Vistaprint-style shopping experience with cart
    - Bulk pricing tiers and testimonials

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
